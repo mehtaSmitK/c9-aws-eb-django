d5373b8182b5c7307bd42eb11040a434e59fddfa
mehtaSmitK <mehta.smit.k@gmail.com>
2021-09-10 19:27:24 +0000

    Fixed main app name issue url

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mysite.views import homePageView
+from main.views import homePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
